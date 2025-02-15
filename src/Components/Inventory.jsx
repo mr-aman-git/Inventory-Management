0171b3044d8dff19fe6c0e0d83e74d1121f6748f
mr-aman-git <amanazr2003@gmail.com>
2025-02-15 20:34:30 +0530

    product list table add

--- src/Components/Inventory.jsx
+++ src/Components/Inventory.jsx
@@ -4,46 +4,70 @@ const Inventory = () => {
   return (
     <>
       <div className="min-h-screen bg-gray-100 p-8">
-        <div className="max-w-[896px] bg-red-400 mx-auto p-5 rounded-md shadow-lg">
+        <div className="max-w-[896px] bg-white mx-auto p-5 rounded-md shadow-lg">
           <h1 className="text-3xl font-semibold text-center mb-5">
             Inventory Management
           </h1>
-          <form action="">
+          <div>
+            <form action="">
+              <div className="mb-6">
+                <input
+                  type="text"
+                  placeholder="Product Name"
+                  name="name"
+                  className="p-2 w-full border-1 rounded-md outline-none"
+                />
+              </div>
 
-            <div className="mb-6">
-              <input
-                type="text"
-                placeholder="Product Name"
-                name="name"
-                className="p-2 w-full border-1 rounded-md outline-none"
-              />
+              <div className="mb-6">
+                <input
+                  type="number"
+                  placeholder="Quantity"
+                  name="number1"
+                  className="p-2 w-full border-1 rounded-md outline-none"
+                />
+              </div>
+
+              <div className="mb-6">
+                <input
+                  type="number"
+                  placeholder="Price"
+                  name="number2"
+                  className="p-2 w-full border-1 rounded-md outline-none"
+                />
+              </div>
+
+              <div className="mb-6">
+                <button className="p-2 w-full border-none rounded-md outline-none bg-blue-500 font-bold text-[17px] cursor-pointer">
+                  Add Product
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div>
+            <h2 className="text-2xl font-semibold mb-5 text-center">
+              Product List
+            </h2>
+            <div>
+              <table className=" w-full border-black border-1 text-center ">
+                <thead>
+                  <tr>
+                    <td className="py-2 px-3 border-1 font-semibold">
+                      Product Name
+                    </td>
+                    <td className="py-2 px-3 border-1 font-semibold">
+                      Quantity
+                    </td>
+                    <td className="py-2 px-3 border-1 font-semibold">Price</td>
+                    <td className="py-2 px-3 border-1 font-semibold">
+                      More Option
+                    </td>
+                  </tr>
+                </thead>
+              </table>
             </div>
-
-            <div className="mb-6">
-              <input
-                type="text"
-                placeholder="Product Name"
-                name="name"
-                className="p-2 w-full border-1 rounded-md outline-none"
-              />
-            </div>
-
-            <div className="mb-6">
-              <input
-                type="text"
-                placeholder="Product Name"
-                name="name"
-                className="p-2 w-full border-1 rounded-md outline-none"
-              />
-            </div>
-
-            <div className="mb-6">
-              <button className="p-2 w-full border-none rounded-md outline-none bg-blue-500 font-bold text-[17px] cursor-pointer">
-                Add Product
-              </button>
-            </div>
-
-          </form>
+          </div>
         </div>
       </div>
     </>
